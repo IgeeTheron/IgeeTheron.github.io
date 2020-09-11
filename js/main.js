70face0956385298ad8416f5c59725055086e98d
Igee Theron <igeetheron@gmail.com>
2020-09-11 17:11:31 +0200

    Fineshed email by using an other service

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Loading tabs
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
 
@@ -26,3 +27,53 @@ function removeShow()
 
 //Listen for tab click
 tabItems.forEach(item => item.addEventListener('click', selectItem));
+//Loading tabs End
+
+//Email
+window.addEventListener("DOMContentLoaded", function() {
+    
+var form = document.getElementById("my-form");
+var status = document.getElementById("status");
+
+// Success and Error functions for after the form is submitted
+
+function success() 
+{
+    form.reset();
+    status.classList.add('success');
+    status.innerHTML = "Thanks!";
+}
+
+function error() 
+{
+    status.classList.add('error');
+    status.innerHTML = "Oops! There was a problem.";
+}
+
+// handle the form submission event
+
+form.addEventListener("submit", function(ev) 
+{
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+});
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+var xhr = new XMLHttpRequest();
+xhr.open(method, url);
+xhr.setRequestHeader("Accept", "application/json");
+xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+    success(xhr.response, xhr.responseType);
+    } else {
+    error(xhr.status, xhr.response, xhr.responseType);
+    }
+};
+xhr.send(data);
+}
+//Email End
